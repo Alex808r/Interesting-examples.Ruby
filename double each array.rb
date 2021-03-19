67e3cb78dc2370bca40a6381c68c606be594ebb4
Alex808r <fromxlam@gmail.com>
2021-03-19 16:54:20 +0300

    add integer

--- double each array.rb
+++ double each array.rb
@@ -9,3 +9,19 @@ array.each do |row|
   print value
   end
 end
+
+puts
+
+array = [ [1, 2], [3, 4], [5, 6] ]
+
+array.each do |item|
+  item.each do |x|
+    print x
+  end
+end
+
+puts
+
+array.each do |item|
+  print item
+end
