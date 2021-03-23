43dd1b78879c9ae638a6e213b504dac79929e6c1
Alex808r <fromxlam@gmail.com>
2021-03-23 22:15:38 +0300

    add array lines

--- fileopen.rb
+++ fileopen.rb
@@ -5,6 +5,8 @@ review_file.close
 #Эквивалентно.
 # Блок открывает файл, помещает его в параметр review_file
 # затем в переменнюу lines сохраняется массив строк по помощи метода .readlines
+lines = [] # переменная объявляется для области видимости иначе к lines
+# не обратиться за пределами блока
 File.open("reviews.txt") do |review_file|
 lines = review_file.readlines
 end
